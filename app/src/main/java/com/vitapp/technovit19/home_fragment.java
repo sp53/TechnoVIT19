c26b1a20a00fa4133879034bb1b5496fa8af7887
Vaibhav Mishra <vaibhavmishra740@gmail.com>
2019-08-06 17:20:48 +0530

    Home

--- app/src/main/java/com/vitapp/technovit19/home_fragment.java
+++ app/src/main/java/com/vitapp/technovit19/home_fragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,11 @@ public class home_fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home,container,false);
+
+        View view = inflater.inflate(R.layout.home,container,false);
+
+        ImageView qubit = view.findViewById(R.id.qubitimage);
+
+        return view;
     }
 }
